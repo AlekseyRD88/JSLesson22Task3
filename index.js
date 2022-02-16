4813027fefcc1bbc2bd1a9cf914ea2b9c98d7afc
= <neymegen50@gmail.com>
2022-02-16 23:54:43 +0200

    message2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inputElem = document.querySelector('.text-input');
-const gettingText = console.log(inputElem.value);
-
-inputElem.addEventListener('change', gettingText);
+function getInput() {
+  console.log(inputElem.value);
+}
+inputElem.addEventListener('change', getInput);
